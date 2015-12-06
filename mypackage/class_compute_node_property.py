9c28d7ecacde406df5d153af81a5ba77f95a9fbb
yuens <ysh329@sina.com>
2015-12-06 16:39:26 +0800

    finished function read_connection_data_in_database(self, database_name, connection_table_name)

--- mypackage/class_compute_node_property.py
+++ mypackage/class_compute_node_property.py
@@ -8,7 +8,7 @@
 
 # Author: Shuai Yuan
 # E-mail: ysh329@sina.com
-# Create: 2015-11-09 13:48:08
+# Create: 2015-12-06 15:22:38
 # Last:
 __author__ = 'yuens'
 
@@ -16,13 +16,9 @@ __author__ = 'yuens'
 import logging
 import MySQLdb
 import time
-import csv
-from pyspark import SparkContext, SparkConf
-from multiprocessing import Pool
-from multiprocessing.dummy import Pool as ThreadPool
 ################################### PART2 CLASS && FUNCTION ###########################
-class CSVDataSaver(object):
-    def __init__(self, database_name):
+class ComputeNodeProperty(object):
+    def __init__(self, database_name, pyspark_sc):
         self.start = time.clock()
 
         logging.basicConfig(level = logging.INFO,
@@ -37,7 +33,7 @@ class CSVDataSaver(object):
         console.setFormatter(formatter)
 
         logging.getLogger('').addHandler(console)
-        logging.info("START CLASS {class_name}.".format(class_name = CSVDataSaver.__name__))
+        logging.info("START CLASS {class_name}.".format(class_name = ComputeNodeProperty.__name__))
 
         # connect database
         try:
@@ -46,6 +42,14 @@ class CSVDataSaver(object):
         except MySQLdb.Error, e:
             logging.error("Fail in connecting MySQL.")
             logging.error("MySQL Error {error_num}: {error_info}.".format(error_num = e.args[0], error_info = e.args[1]))
+
+        # spark configure
+        try:
+            self.sc = pyspark_sc
+            logging.info("Config spark successfully.")
+        except Exception as e:
+            logging.error("Config spark failed.")
+            logging.error(e)
 
 
 
@@ -57,36 +61,96 @@ class CSVDataSaver(object):
         except MySQLdb.Error, e:
             logging.error("MySQL Error {error_num}: {error_info}.".format(error_num = e.args[0], error_info = e.args[1]))
 
-        logging.info("END CLASS {class_name}.".format(class_name = CSVDataSaver.__name__))
+        logging.info("END CLASS {class_name}.".format(class_name = ComputeNodeProperty.__name__))
         self.end = time.clock()
-        logging.info("The class {class_name} run time is : {delta_time} seconds".format(class_name = CSVDataSaver.__name__, delta_time = self.end))
+        logging.info("The class {class_name} run time is : {delta_time} seconds".format(class_name = ComputeNodeProperty.__name__, delta_time = self.end))
+
+
+
+    def read_connection_data_in_database(self, database_name, connection_table_name):
+        cursor = self.con.cursor()
+        sqls = []
+        sqls.append("""USE {0}""".format(database_name))
+        sqls.append("""SELECT network_type, is_directed, connection_id, node1, node2 FROM {database}.{table}"""\
+                    .format(database = database_name, table = connection_table_name)\
+                    )
+
+        for sql_idx in xrange(len(sqls)):
+            sql = sqls[sql_idx]
+            try:
+                cursor.execute(sql)
+                if sql_idx == len(sqls)-1:
+                    connection_data_2d_tuple = cursor.fetchall()
+                    logging.info("len(connection_data_2d_tuple):{0}".format(len(connection_data_2d_tuple)))
+                    logging.info("type(connection_data_2d_tuple):{0}".format(type(connection_data_2d_tuple)))
+                    logging.info("connection_data_2d_tuple[:10]:{0}".format(connection_data_2d_tuple[:10]))
+                    logging.info("type(connection_data_2d_tuple[0]):{0}".format(type(connection_data_2d_tuple[0])))
+            except MySQLdb.Error, e:
+                self.con.rollback()
+                logging.error("Fail in attaining connection data from {database_name}.{table_name}.".format(database_name = database_name, table_name = connection_table_name))
+                logging.error("MySQL Error {error_num}: {error_info}.".format(error_num = e.args[0], error_info = e.args[1]))
+                return 0
+        cursor.close()
+
+        # transform to rdd
+        connection_data_tuple_rdd = self.sc.parallelize(connection_data_2d_tuple)\
+                                           .map(lambda (network_type, is_directed, connection_id, node1, node2):\
+                                                                                        (network_type.encode("utf8"),\
+                                                                                         int(is_directed),\
+                                                                                         int(connection_id),\
+                                                                                         int(node1),\
+                                                                                         int(node2))\
+                                                )
+        logging.info("connection_data_tuple_rdd.count():{0}".format(connection_data_tuple_rdd.count()))
+        logging.info("connection_data_tuple_rdd.take(3):{0}".format(connection_data_tuple_rdd.take(3)))
+
+
+        # transform to six different rdd
+        ## bio network
+        bio_directed_tuple_rdd = connection_data_tuple_rdd.filter(lambda (network_type, is_directed, connection_id, node1, node2): network_type == "bio" and is_directed == 0)
+        logging.info("bio_directed_tuple_rdd.count():{0}".format(bio_directed_tuple_rdd.count()))
+        logging.info("bio_directed_tuple_rdd.take(3):{0}".format(bio_directed_tuple_rdd.take(3)))
+
+        bio_undirected_tuple_rdd = connection_data_tuple_rdd.filter(lambda (network_type, is_directed, connection_id, node1, node2): network_type == "bio" and is_directed == 1)
+        logging.info("bio_undirected_tuple_rdd.count():{0}".format(bio_undirected_tuple_rdd.count()))
+        logging.info("bio_undirected_tuple_rdd.take(3):{0}".format(bio_undirected_tuple_rdd.take(3)))
+
+        # info network
+        info_directed_tuple_rdd = connection_data_tuple_rdd.filter(lambda (network_type, is_directed, connection_id, node1, node2): network_type == "info" and is_directed == 0)
+        logging.info("info_directed_tuple_rdd.count():{0}".format(info_directed_tuple_rdd.count()))
+        logging.info("info_directed_tuple_rdd.take(3):{0}".format(info_directed_tuple_rdd.take(3)))
+
+        info_undirected_tuple_rdd = connection_data_tuple_rdd.filter(lambda (network_type, is_directed, connection_id, node1, node2): network_type == "info" and is_directed == 1)
+        logging.info("info_undirected_tuple_rdd.count():{0}".format(info_undirected_tuple_rdd.count()))
+        logging.info("info_undirected_tuple_rdd.take(3):{0}".format(info_undirected_tuple_rdd.take(3)))
+
+        # social network
+        social_directed_tuple_rdd = connection_data_tuple_rdd.filter(lambda (network_type, is_directed, connection_id, node1, node2): network_type == "social" and is_directed == 0)
+        logging.info("social_directed_tuple_rdd.count():{0}".format(social_directed_tuple_rdd.count()))
+        logging.info("social_directed_tuple_rdd.take(3):{0}".format(social_directed_tuple_rdd.take(3)))
+
+        social_undirected_tuple_rdd = connection_data_tuple_rdd.filter(lambda (network_type, is_directed, connection_id, node1, node2): network_type == "social" and is_directed == 1)
+        logging.info("social_undirected_tuple_rdd.take(3):{0}".format(social_undirected_tuple_rdd.count()))
+        logging.info("social_undirected_tuple_rdd.take(3):{0}".format(social_undirected_tuple_rdd.take(3)))
+
+        network_rdd_list = [bio_directed_tuple_rdd, bio_undirected_tuple_rdd,\
+                            info_directed_tuple_rdd, info_undirected_tuple_rdd,\
+                            social_directed_tuple_rdd, social_undirected_tuple_rdd]
+        return network_rdd_list
 
 
 
     def compute_node_in_different_network(self):
         pass
 
+
 ################################### PART3 CLASS TEST ##################################
 """
 # Initialization
 database_name = "LinkPredictionDB"
 connection_table_name = "connection_table"
-network_name_list = ["../data/input/Bio-network_directed.csv", "../data/input/Bio-network_undirected.csv",\
-                     "../data/input/Info-network_directed.csv", "../data/input/Info-network_undirected.csv",\
-                     "../data/input/Social network_directed.csv", "../data/input/Social network_undirected.csv"]
 
-network_type_list = ["bio", "bio",\
-                     "info", "info",\
-                     "social", "social"]
-
-is_directed_list = [0, 1,\
-                    0, 1,\
-                    0, 1]
-
-CSVReader = CSVDataSaver(database_name = database_name)
-CSVReader.read_csv_to_database(database_name = database_name,\
-                               connection_table_name = connection_table_name,\
-                               network_name_list = network_name_list,\
-                               network_type_list = network_type_list,\
-                               is_directed_list = is_directed_list)
+Computer = ComputeNodeProperty(database_name = database_name)
+Computer.read_connection_data_in_database(database_name = database_name,\
+                                          connection_table_name = connection_table_name)
 """
